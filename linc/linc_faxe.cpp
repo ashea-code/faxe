8d903c5cfd686ddeb26510787976263a9ad48b2a
AaronShea <aaron@aaronshea.me>
2016-10-03 10:00:07 -0400

    2016

--- linc/linc_faxe.cpp
+++ linc/linc_faxe.cpp
@@ -3,7 +3,7 @@
 *
 * The MIT License (MIT)
 *
-* Copyright (c) 2015 Aaron M. Shea
+* Copyright (c) 2016 Aaron M. Shea
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
