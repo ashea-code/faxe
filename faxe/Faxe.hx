f2b04e2d848efd977cc092ca8a361465be17c41c
AaronShea <aaron@aaronshea.me>
2016-10-06 07:48:28 -0400

    add bindings to haxe for loading/unloading sounds

--- faxe/Faxe.hx
+++ faxe/Faxe.hx
@@ -15,4 +15,10 @@ extern class Faxe
 
 	@:native("linc::faxe::faxe_unload_bank")
 	public static function fmod_unload_bank(bankFilePath:String):Void;
+
+	@:native("linc::faxe::faxe_load_sound")
+	public static function fmod_load_sound(soundPath:String, looping:Bool = false, streaming:Bool = false):Void;
+
+	@:native("linc::faxe::faxe_unload_sound")
+	public static function fmod_unload_sound(bankFilePath:String):Void;
 }
